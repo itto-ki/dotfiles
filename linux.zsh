833b5e025c9d0d5e82dc08608b3a4949f509e19f
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2018-11-15 01:30:24 +0900

    Update: settings for GCP

--- linux.zsh
+++ linux.zsh
@@ -34,3 +34,12 @@ fi
 # pipenv
 eval "$(pipenv --completion)"
 export PIPENV_VENV_IN_PROJECT=true
+
+############################################################################################
+### Google Colud Platform SDK
+############################################################################################
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/google-cloud-sdk/path.zsh.inc' ]; then . '/opt/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/opt/google-cloud-sdk/completion.zsh.inc' ]; then . '/opt/google-cloud-sdk/completion.zsh.inc'; fi
