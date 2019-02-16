c6699b4d6a513153814ca6786b91debb7f9ec086
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-02-17 04:07:07 +0900

    BugFix: Rust-lang toolchain path

--- linux.zsh
+++ linux.zsh
@@ -21,7 +21,7 @@ alias jupyter=$HOME/anaconda3/bin/jupyter
 ### Rust
 ############################################################################################
 # completion
-export RUST_SRC_PATH=$HOME/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
+export RUST_SRC_PATH=$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 
 ############################################################################################
 ### Pyenv & Pipenv
