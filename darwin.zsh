2867c67663c0ccc74d451d7b302c3a78c6462786
Itto Kimura <itto-kimura@m3.com>
2019-05-21 11:59:31 +0900

    Add: Python & Golang settings

--- darwin.zsh
+++ darwin.zsh
@@ -6,3 +6,20 @@ export EDITOR=nvim
 export LANG=ja_JP.UTF-8
 export LESSOPEN='| source-highlight %s'
 export RUST_SRC_PATH=$HOME/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
+export GOPATH=$HOME/go
+export PATH=$PATH:${GOPATH//://bin:}/bin
+export PATH=$HOME/google-cloud-sdk/bin:$PATH
+
+############################################################################################
+### Pyenv & Pipenv
+############################################################################################
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+# pipenv
+eval "$(pipenv --completion)"
+export PIPENV_VENV_IN_PROJECT=true
+
