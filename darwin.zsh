c4d636f2da5597bce10676ab6285bff61c755b01
Itto Kimura <itto-kimura@m3.com>
2019-05-23 17:22:11 +0900

    Delete: a garbage setting

--- darwin.zsh
+++ darwin.zsh
@@ -4,7 +4,6 @@
 export PATH=$HOME/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/texbin:$HOME/.cargo/bin:$PATH
 export EDITOR=nvim
 export LANG=ja_JP.UTF-8
-export LESSOPEN='| source-highlight %s'
 export RUST_SRC_PATH=$HOME/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
 export GOPATH=$HOME/go
 export PATH=$PATH:${GOPATH//://bin:}/bin
