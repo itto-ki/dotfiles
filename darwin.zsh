cada419ad52453a197cba478e4594bbb34533023
Itto Kimura <itto-kimura@m3.com>
2019-06-03 16:28:11 +0900

    Delete: machine specifly settings

--- darwin.zsh
+++ darwin.zsh
@@ -1,7 +1,7 @@
 ############################################################################################
 ### Environmental variables
 ############################################################################################
-export PATH=$HOME/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/texbin:$HOME/.cargo/bin:$PATH
+export PATH=/usr/texbin:$HOME/.cargo/bin:$PATH
 export EDITOR=nvim
 export LANG=ja_JP.UTF-8
 export RUST_SRC_PATH=$HOME/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
