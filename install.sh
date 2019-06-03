a873f9a2144889f6784f7fba199627a0b34c7201
Itto Kimura <itto-kimura@m3.com>
2019-06-03 10:10:45 +0900

    Add: settings of installing vim-plug

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 NVIMDIR=$HOME/.config/nvim
 NVIM_PLUGIN_DIR=$NVIMDIR/plugin
-NVIMFILES=(init.vim dein.toml dein_lazy.toml)
+NVIM_CONF_FILE=init.vim
 GTAGS_VIM_SCRIPT=https://gist.githubusercontent.com/itto-ki/ef1606ea4120ed44383e1eb1518d12c7/raw/2d04567041532db9f18f767175b31911ae4faac0/gtags.vim
 DEIN_INSTALLSCRIPT=dein_installer.sh
 CONFIGFIELS=(zshrc tmux.conf gitconfig)
@@ -17,12 +17,15 @@ fi
 if [ ! -e $NVIM_PLUGIN_DIR ]; then
     mkdir -p $NVIM_PLUGIN_DIR
 fi
+
+# vim-plugのインストール
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # neovimの設定ファイルを作成
-for file in ${NVIMFILES[@]}; do
-    if [ ! -e $NVIMDIR/$file ]; then
-        ln -s $SCRIPT_DIR/$file $NVIMDIR/$file
-    fi
-done
+if [ ! -e $NVIMDIR/$NVIM_CONF_FILE ]; then
+    ln -s $SCRIPT_DIR/$NVIM_CONF_FILE $NVIMDIR/$NVIM_CONF_FILE
+fi
 
 # GNU GlobalのためのVim Pluginを追加
 curl $GTAGS_VIM_SCRIPT > $NVIM_PLUGIN_DIR/gtags.vim
